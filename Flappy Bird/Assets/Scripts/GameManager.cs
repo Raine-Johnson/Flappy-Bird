148f32b3e4901698eec848ce0e8b6547efafb924
Raine <raine.johnson@digipen.edu>
2020-01-30 09:36:09 -0800

    Added some main menu stuff

--- Flappy Bird/Assets/Scripts/GameManager.cs
+++ Flappy Bird/Assets/Scripts/GameManager.cs
@@ -7,8 +7,6 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        GameObject gameObject = new GameObject("Pipe", typeof(SpriteRenderer));
-        gameObject.GetComponent<SpriteRenderer>().sprite = GameAssets.GetInstance().pipeHeadSprite;
         
     }
 
