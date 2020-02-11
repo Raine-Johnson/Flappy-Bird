81af9b356dffc22788d8d09e3dabb98c6b4c19a3
Raine <raine.johnson@digipen.edu>
2020-02-11 09:22:46 -0800

    Almost have tilting done

--- Flappy Bird/Assets/Scripts/TiltWithYVector.cs
+++ Flappy Bird/Assets/Scripts/TiltWithYVector.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class TiltWithYVector : MonoBehaviour
 {
+    public Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float angle = Vector3.Angle(Vector3.right, rigidbody.velocity);
+        if (rigidbody.velocity.y < 0)
+        {
+            angle = -angle;
+        }
+
+        transform.eulerAngles = new Vector3(90, 180, angle);
     }
+    
 }
